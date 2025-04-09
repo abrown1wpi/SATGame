acaf73d6ab87dfa641eddf0d358f4f1420558ac9
abrown1wpi <andrewbrown2026@gmail.com>
2025-04-09 08:06:30 -0400

    New questions

---
+++ Scripts/char_in_fight.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+var stats : Char_Fight
+var sprite_path : String
+@onready var char_ani = $"Control/CharAni"
+
+func _init(attack_strength, health, sprite_path_in) -> void:
+	stats = Char_Fight.new()
+	stats.attack_mult = attack_strength
+	stats.health = health
+	
+	sprite_path = sprite_path_in
+	
+func _ready() -> void:
+	char_ani.sprite_frames = load(sprite_path)
+	
--- Scripts/question_handler.gd
+++ Scripts/question_handler.gd
@@ -1,7 +1,7 @@
 extends Resource
 class_name Question_Handler
 
-var question_data_path = "res://Data/gamespread.json"
+var question_data_path = "res://Data/questionsJSON.json"
 
 var questions : Dictionary
 
@@ -9,19 +9,21 @@ func fight_start():
 	var json_data = UtilFuncs.load_json_from_path(question_data_path)
 	
 	if json_data != null:
-		var question_data = json_data.get("questions")
+		var question_data = json_data.get("Questions")
 		if question_data != null:
 			for i in range(0, question_data.size()):
 				questions[i] = parse_question_data(i, question_data[str(i)])
+	else:
+		print("No data")
 			
 			
-func parse_question_data(_id, json_data : Dictionary):
+func parse_question_data(id, json_data : Dictionary):
 	var question_data : QuestionData = QuestionData.new()
 	
-	question_data.answer = json_data["C_ANSWER"]
+	question_data.answer = str(json_data["C_ANSWER"])
 	question_data.question = json_data["QUESTION"]
 	
-	question_data.incorrect_answers = [json_data["ANSWER_1"], json_data["ANSWER_2"], json_data["ANSWER_3"]]
+	question_data.incorrect_answers = [str(json_data["ANSWER_1"]), str(json_data["ANSWER_2"]), str(json_data["ANSWER_3"])]
 	
 	return question_data
 	
