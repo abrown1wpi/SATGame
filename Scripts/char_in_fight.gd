0438edf7c9ac85f6b1fe5223943973018bd0fbb8
abrown1wpi <andrewbrown2026@gmail.com>
2025-04-23 10:35:01 -0400

    Ravena aniamtion tutorial

--- Scripts/char_in_fight.gd
+++ Scripts/char_in_fight.gd
@@ -2,13 +2,14 @@ extends Node2D
 
 var stats : Char_Fight
 var sprite_path : String
+var cur_ani : String
 @onready var char_ani = $"Control/CharAni"
 
 func _ready():
-	pass
+	cur_ani = "idle"
 	
 func _process(delta):
-	pass
+	char_ani.play(cur_ani)
 
 func init(attack_strength : int, health: int, sprite_path_in: String) -> void:
 	stats = Char_Fight.new()
@@ -18,3 +19,6 @@ func init(attack_strength : int, health: int, sprite_path_in: String) -> void:
 	sprite_path = sprite_path_in
 	var frames_resource = load(sprite_path) as SpriteFrames
 	char_ani.frames = frames_resource
+
+func play_death():
+	cur_ani = "death"
