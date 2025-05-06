d4c803df28e8f571900b43a715f69466188a59e0
abrown1wpi <andrewbrown2026@gmail.com>
2025-05-06 08:19:18 -0400

    health bar

--- Scenes/Fight/health_bar_handler.gd
+++ Scenes/Fight/health_bar_handler.gd
@@ -3,14 +3,17 @@ extends Control
 @onready var heart_node = hearts_parent.get_child(0)
 
 # Called when the node enters the scene tree for the first time.
+var health : int
 	
-func init(health : int, pc : int):
+func init(h : int, pc : int):
 	hearts_parent.scale = Vector2(5, 5)
+	health = h
 	for i in range(health):
 		heart_node = hearts_parent.get_child(i)
 		var dupe_heart = heart_node.duplicate()
 		hearts_parent.add_child(dupe_heart)
 	
 	if (pc == 1):
-		hearts_parent.anchor_right()
+		var last_heart = hearts_parent.get_child(health - 1)
+		
 		
